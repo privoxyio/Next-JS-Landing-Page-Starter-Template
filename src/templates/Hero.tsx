8ff0c7abfc2d6891499c8b94a60108d8d47f4be8
Alex  Maynard <contact@privoxy.io>
2023-04-11 23:24:24 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,13 +12,13 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
+          <Link href="https://shop.privoxy.io">
+            <a>Store</a>
           </Link>
         </li>
         <li>
-          <Link href="/">
-            <a>Sign in</a>
+          <Link href="https://shop.privoxy.io/about-us/">
+            <a>About Us</a>
           </Link>
         </li>
       </NavbarTwoColumns>
@@ -28,15 +28,15 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {'The worlds largest electronics superstore powered by\n'}
+            <span className="text-primary-500">Cryptocurrency Payments</span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="Unbeatable discounts and ultimate privacy protection."
         button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
+          <Link href="https://shop.privoxy.io">
             <a>
-              <Button xl>Download Your Free Theme</Button>
+              <Button xl>Shop Now</Button>
             </a>
           </Link>
         }
