3dceae427a1e84590680e420ad02d8fb97f3bd55
Alex  Maynard <contact@privoxy.io>
2023-04-11 22:02:57 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -86,8 +86,8 @@ const Footer = () => (
           </Link>
         </li>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
+          <Link href="https://shop.privoxy.io/about-us/">
+            <a>About Us</a>
           </Link>
         </li>
       </CenteredFooter>
