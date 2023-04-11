5dae5ec83474e92113e0a65f884fe20c6b902780
Alex  Maynard <contact@privoxy.io>
2023-04-11 21:48:16 +0000

    Updated 1 file via CloudCannon.

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Privxoy',
+  title: 'Privoxy.io Store',
+  description: 'The best place to spend your crypto',
   locale: 'en',
 };
