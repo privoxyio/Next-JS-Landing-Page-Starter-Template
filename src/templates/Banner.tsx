692e87d0ddadf73f466adfbf7e60869e19a3b010
Alex  Maynard <contact@privoxy.io>
2023-04-12 06:19:18 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -7,7 +7,7 @@ import { Section } from '../layout/Section';
 const Banner = () => (
   <Section>
     <CTABanner
-      title="Ready for deep crypto discounts?"
+      title="Ready for DEEP crypto discounts?"
       subtitle="Save 10% today"
       button={
         <Link href="https://shop.privoxy.io">
