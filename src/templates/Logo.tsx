439c31c653570ae8c41079205cb6b2059914dd29
Privoxy Solutions <contact@privoxy.io>
2023-04-12 07:03:54 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -24,7 +24,7 @@ const Logo = (props: ILogoProps) => {
         strokeLinejoin="round"
       >
         <path d="M0 0h24v24H0z" stroke="none" />
-        <image href="https://shop.privoxy.io/wp-content/uploads/2023/03/cropped-PNG-FILE-HIGH-RESOLUTION-01-1024x664.png" width="149" height="96" />
+        <image href="https://privoxy.io/public/assets/images/logo.png" width="149" height="96" />
       </svg>
 
       {AppConfig.site_name}
