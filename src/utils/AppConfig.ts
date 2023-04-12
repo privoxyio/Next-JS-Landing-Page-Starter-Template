92805bc7679f6789cd7dca033a666412a804a4e5
Alex  Maynard <contact@privoxy.io>
2023-04-12 06:29:24 +0000

    Updated 1 file via CloudCannon.

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: '',
   title: 'Privoxy',
-  description: 'Tech-focused e-retailer powered by cryptocurrency payments.',
+  description: 'We are a tech-focused e-retailer powered by cryptocurrency payments.',
   locale: 'en',
 };
