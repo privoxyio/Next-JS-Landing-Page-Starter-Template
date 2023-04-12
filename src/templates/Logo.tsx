ff2ac15670fd9d07874b9f367d422832c6443510
Alex  Maynard <contact@privoxy.io>
2023-04-12 01:44:56 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -17,7 +17,7 @@ const Logo = (props: ILogoProps) => {
         xmlns="http://www.w3.org/2000/svg"
         width={size}
         height={size}
-        viewBox="0 0 24 24"
+        viewBox="0 0 48 48"
         strokeWidth="1.5"
         fill="none"
         strokeLinecap="round"
