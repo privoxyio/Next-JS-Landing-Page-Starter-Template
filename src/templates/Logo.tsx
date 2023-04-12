324c2b0f6051b402f07861820d49b635b88f8aad
Privoxy Solutions <contact@privoxy.io>
2023-04-12 07:06:06 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -5,7 +5,7 @@ type ILogoProps = {
 };
 
 const Logo = (props: ILogoProps) => {
-  const size = props.xl ? '149' : '96';
+  const size = props.xl ? '153' : '103';
   const fontStyle = props.xl
     ? 'font-semibold text-3xl'
     : 'font-semibold text-xl';
@@ -17,14 +17,14 @@ const Logo = (props: ILogoProps) => {
         xmlns="http://www.w3.org/2000/svg"
         width={size}
         height={size}
-        viewBox="0 0 149 96"
+        viewBox="0 0 153 103"
         strokeWidth="1.5"
         fill="none"
         strokeLinecap="round"
         strokeLinejoin="round"
       >
         <path d="M0 0h24v24H0z" stroke="none" />
-        <image href="https://privoxy.io/public/assets/images/logo.png" width="149" height="96" />
+        <image href="https://privoxy.io/public/assets/images/logo.png" width="153" height="103" />
       </svg>
 
       {AppConfig.site_name}
