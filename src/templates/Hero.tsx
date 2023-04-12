484354aa6bc3e95d614789c52414e49362276541
Privoxy Solutions <contact@privoxy.io>
2023-04-12 06:40:24 +0000

    Updated 1 file via CloudCannon.

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'We are a Tech-focused e-retailer powered by\n'}
+            {'We are a tech-focused e-retailer powered by\n'}
             <span className="text-primary-500">cryptocurrency payments</span>
           </>
         }
