d137f646cc4f6e35e7c9cd56844f4a439fa50ccd
Privoxy Solutions <contact@privoxy.io>
2023-04-19 02:16:30 +0000

    Updated 1 file via CloudCannon.

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -17,14 +17,14 @@ const Meta = (props: IMetaProps) => {
     <>
       <Head>
         <script>
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3457043,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3457043,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         </script>
         <meta charSet="UTF-8" key="charset" />
         <meta
